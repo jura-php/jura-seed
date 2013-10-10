c8975c86d75ba5480bd4b016f37a6dcca46d28ff
Ramon Fritsch <ramon@joy-interactive.com>
2013-10-10 10:13:05 -0300

    index.php mais rápido, git ignores corrigidos, system atualizado

--- index.php
+++ index.php
@@ -4,11 +4,13 @@ define("DS", DIRECTORY_SEPARATOR);
 define("EXT", ".php");
 define("CRLF", PHP_EOL);
 
+$dir = realpath(__DIR__);
+
 define("J_START", microtime(true));
-define("J_PATH", realpath(__DIR__) . DS);
-define("J_APPPATH", realpath(__DIR__)  . DS . "app" . DS);
-define("J_MANAGERPATH", realpath(__DIR__)  . DS . "manager" . DS);
-define("J_SYSTEMPATH", realpath(__DIR__)  . DS . "system" . DS);
+define("J_PATH", $dir . DS);
+define("J_APPPATH", $dir . DS . "app" . DS);
+define("J_MANAGERPATH", $dir . DS . "manager" . DS);
+define("J_SYSTEMPATH", $dir . DS . "system" . DS);
 define("J_LOCAL_ENV", "local");
 define("J_PREVIEW_ENV", "preview");
 
